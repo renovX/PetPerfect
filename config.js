f03e9dc7c8550b84e0e5ef0d4fe0b71e38f86fca
renovX <78961109+renovX@users.noreply.github.com>
2023-03-15 03:11:05 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-await mongoose.connect(
-  "mongodb+srv://lasty401:foolishdon@cluster0.vzeau1t.mongodb.net/sample"
-);
-const db = mongoose.connection;
-export default db;
